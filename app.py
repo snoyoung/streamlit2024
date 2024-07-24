3a254fb5321d49efad79340633013d946aacfa54
snoyoung <snoyoung@naver.com>
2024-07-24 11:51:29 +0900

    app수정

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import random 
 
-st.write('Hello Streamlit!')
-st.write('My name is **김유경**')
+def show() :
+  st.header('주사위 게임', divider='rainbow')
+  clicked = st.button('주사위던지기', type='primary')
+  if clicked :
+    n = random.randint(1, 6)
+    st.image(f'./img/{n}.png')
